9b0122a4f6698a4da21cb2fb2b29a6dffd57af70
Mehulpathak12 <mehulpathak48@gmail.com>
2025-07-03 20:51:30 +0530

    update

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,23 +1,12 @@
 const { createClient } = require('redis');
 const redis = createClient({
-    url: process.env.REDIS_AIVEN,
-    socket: {
-      tls: true, // ✅ This is critical for Aiven
-      rejectUnauthorized: false // optional: only if you get TLS cert errors
-    }
-  });
-
-// createClient({
-//   username: process.env.REDIS_USERNAME,
-//   password: process.env.REDIS_PASSWORD,
-//   socket: {
-//       host: process.env.REDIS_HOST,
-//       port: process.env.REDIS_PORT
-//   }
-// }); 
-
-
-
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT
+  }
+}); 
 redis.on('error', err => console.error('Redis Client Error', err));
 
 redis.connect().then(console.log("Redis connected"));
